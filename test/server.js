a23a8ab2313b609d52dc3ba539bf46372ba12a76
Melody <no30131@gmail.com>
2024-06-09 22:44:38 +0800

    [test] image contour recognition based on color2

--- test/server.js
+++ test/server.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const multer = require('multer');
 const axios = require('axios');
@@ -6,111 +7,50 @@ const path = require('path');
 
 const app = express();
 const port = 3000;
+app.use(express.static("public"));
 
-// 中間件來解析 JSON 請求
 app.use(express.json());
-
-// 設置上傳目錄
-// const upload = multer({ dest: 'uploads/' });
 const upload = multer({
   storage: multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, 'uploads/')
     },
     filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now())
+      cb(null, file.originalname)
     }
-  }),
-  fileFilter: function (req, file, cb) {
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-      cb(null, true)
-    } else {
-      cb(new Error('Only JPEG and PNG images are allowed'), false)
-    }
-  }
-}, { dest: 'uploads/' }) // 这里可以省略，因为我们已经在 storage 中定义了 dest
-
+  })
+});
 
 app.post('/upload', upload.single('image'), async (req, res) => {
   try {
-    // 讀取上傳的圖片文件
     const image = fs.readFileSync(req.file.path);
     const encodedImage = image.toString('base64');
+    const colors = ['red', 'purple', 'green', 'black', 'yellow'];
+    const processedImages = {};
 
-    // 發送圖片到 Python 伺服器
-    const response = await axios.post('http://localhost:5000/process_image', {
-      image: encodedImage
-    });
+    for (const color of colors) {
+      const response = await axios.post('http://localhost:5000/select_rock_point', {
+        color,
+        image: encodedImage
+      });
+      const { image, filename } = response.data;
+      const imagePath = `/uploads/${filename}`;
+      fs.writeFileSync(path.join(__dirname, 'uploads', filename), Buffer.from(image, 'base64'));
+      processedImages[color] = imagePath;
+    }
 
-    // 從 Python 伺服器接收處理後的圖片
-    const processedImage = response.data.image;
-
-    // 生成唯一的文件名
-    const uniqueFilename = generateUniqueFilename(req.file.originalname);
-
-    // 將處理後的圖片保存到 uploads 資料夾
-    const imagePath = path.join(__dirname, 'uploads', uniqueFilename);
-    fs.writeFileSync(imagePath, processedImage, 'base64');
-
-    // 發送處理後的圖片路徑給前端
-    res.json({ imagePath });
-    // res.json({ imagePath: `/uploads/${uniqueFilename}` });
+    res.json({ images: processedImages });
   } catch (error) {
     console.error('Error processing image:', error);
     res.status(500).send('Error processing image');
   } finally {
-    // 刪除臨時上傳的文件
     fs.unlinkSync(req.file.path);
   }
 });
 
-
-app.post('/select_color', async (req, res) => {
-  try {
-    const { color } = req.body;
-    // 發送選擇的顏色到 Python 伺服器
-    const response = await axios.post('http://localhost:5000/select_rock_point', { color });
-
-    // // 從 Python 伺服器接收處理後的圖片
-    // const processedImage = response.data.image;
-    // 從 Python 伺服器接收處理後的圖片和文件名
-    const { image, filename } = response.data;
-
-    // // 生成唯一的文件名
-    // const uniqueFilename = generateUniqueFilename(`selected_${color}.png`);
-
-    // // 將處理後的圖片保存到 uploads 資料夾
-    // const imagePath = path.join(__dirname, 'uploads', uniqueFilename);
-    // fs.writeFileSync(imagePath, processedImage, 'base64'); // 解碼並保存圖片
-    
-    // // 將處理後的圖片保存到 uploads 資料夾
-    // const imagePath = path.join(__dirname, 'uploads', filename);
-    // fs.writeFileSync(imagePath, image, 'base64'); // 解碼並保存圖片
-    
-    // // 發送處理後的圖片路徑給前端
-    // res.json({ imagePath }); // 返回相對路徑
-    
-    // 直接返回处理后的图像数据，无需保存到文件系统
-    res.json({ image, filename });
-  } catch (error) {
-    console.error('Error processing image:', error);
-    res.status(500).send('Error processing image');
-  }
-});
-
-// 設置靜態文件夾以提供處理後的圖片
+// 設置靜態文件夾來提供處理後的圖片
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use(express.static("public"));
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
-
-// 生成唯一的文件名
-function generateUniqueFilename(originalFilename) {
-    const currentDateTime = new Date().toISOString().replace(/[-:]/g, '').replace(/\..+/, '');
-    const randomSuffix = Math.random().toString(36).substring(2, 5);
-    const fileExtension = path.extname(originalFilename);
-    const newFilename = `${path.basename(originalFilename, fileExtension)}_${currentDateTime}_${randomSuffix}${fileExtension}`;
-    return newFilename;
-  }
--- test/server.py
+++ test/server.py
@@ -1,126 +1,3 @@
-# from flask import Flask, request, jsonify
-# import cv2
-# import numpy as np
-# import base64
-
-# app = Flask(__name__)
-
-# @app.route('/process_image', methods=['POST'])
-# def process_image():
-#     try:
-#         data = request.json
-#         encoded_image = data['image']
-        
-#         # 解码图像
-#         decoded_image = base64.b64decode(encoded_image)
-#         np_img = np.frombuffer(decoded_image, np.uint8)
-#         img = cv2.imdecode(np_img, cv2.IMREAD_COLOR)
-        
-#         # 保存原始圖像以供後續使用
-#         cv2.imwrite('original_image.png', img)
-
-#         # 转换为 HSV 色彩空间
-#         hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        
-#         # # 進行形態學梯度運算
-#         # ksize = (5, 5)
-#         # M = cv2.getStructuringElement(cv2.MORPH_CROSS, ksize)
-#         # dst = cv2.morphologyEx(hsv_img, cv2.MORPH_GRADIENT, M)
-        
-#         # 定义颜色范围
-#         lower_color = np.array([40, 50, 50])
-#         upper_color = np.array([90, 255, 255])
-        
-#         # 过滤岩点
-#         color_mask = cv2.inRange(hsv_img, lower_color, upper_color)
-#         color_points = cv2.bitwise_and(img, img, mask=color_mask)
-        
-#         # 在透明背景图像上繪製描邊框線
-#         contours, _ = cv2.findContours(color_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-#         cv2.drawContours(color_points, contours, -1, (255, 0, 0), thickness=2)
-        
-#         # 创建透明背景图像
-#         # transparent_img = np.zeros((dst.shape[0], dst.shape[1], 4), dtype=np.uint8)
-#         transparent_img = np.zeros((img.shape[0], img.shape[1], 4), dtype=np.uint8)
-
-#         # 将岩点和框线添加到透明背景图像
-#         transparent_img[:, :, :3] = color_points
-#         transparent_img[:, :, 3] = cv2.bitwise_or(color_mask, color_mask)
-        
-#         # 将透明背景图像叠加到原始图像上
-#         overlay = cv2.addWeighted(img, 0.75, transparent_img[:, :, :3], 0.5, 0)
-        
-#         # 将处理后的图像转换为 base64 字符串
-#         _, encoded_processed_image = cv2.imencode('.png', overlay)
-#         processed_image = base64.b64encode(encoded_processed_image).decode('utf-8')
-
-#         return jsonify({'image': processed_image})
-#     except Exception as e:
-#         print("Error processing image:", e)
-#         return jsonify({'error': 'Error processing image'})
-
-# @app.route('/select_rock_point', methods=['POST'])
-# def select_rock_point():
-#     try:
-#         data = request.json
-#         color = data['color']
-        
-#         # 定義顏色範圍，這裡需要你之前提供的顏色範圍
-#         color_ranges = {
-#             'red': ([0, 100, 100], [20, 255, 255]),
-#             'blue': ([64, 0, 192], [192, 64, 255]),
-#             'yellow': ([192, 192, 0], [255, 255, 64]),
-#             'green': ([0, 192, 0], [128, 255, 128]),
-#             'black': ([0, 0, 0], [64, 64, 64]),
-#             'orange': ([192, 64, 0], [255, 128, 0]),
-#             'purple': ([128, 0, 128], [192, 64, 192]),
-#             'bright_orange': ([255, 165, 0], [255, 200, 0]),
-#             'dark_green': ([0, 100, 0], [0, 128, 0]),
-#             'beige': ([245, 245, 220], [255, 255, 240])
-#         }
-        
-#         if color not in color_ranges:
-#             return jsonify({'error': 'Invalid color selected'}), 400
-
-#         lower_color, upper_color = color_ranges[color]
-#         lower_color = np.array(lower_color)
-#         upper_color = np.array(upper_color)
-        
-#         # 讀取原始圖像
-#         img = cv2.imread('original_image.png')
-#         hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        
-#         # 過濾指定顏色的岩點
-#         color_mask = cv2.inRange(hsv_img, lower_color, upper_color)
-#         color_points = cv2.bitwise_and(img, img, mask=color_mask)
-        
-#         # 在透明背景圖像上繪製描邊框線
-#         contours, _ = cv2.findContours(color_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-#         cv2.drawContours(color_points, contours, -1, (255, 0, 0), thickness=2)
-        
-#         # 創建透明背景圖像
-#         transparent_img = np.zeros((img.shape[0], img.shape[1], 4), dtype=np.uint8)
-        
-#         # 將岩點和框線添加到透明背景圖像
-#         transparent_img[:, :, :3] = color_points
-#         transparent_img[:, :, 3] = cv2.bitwise_or(color_mask, color_mask)
-        
-#         # 將透明背景圖像疊加到原始圖像上
-#         overlay = cv2.addWeighted(img, 0.75, transparent_img[:, :, :3], 0.5, 0)
-        
-#         # 將處理後的圖像轉換為base64字符串
-#         _, encoded_processed_image = cv2.imencode('.png', overlay)
-#         processed_image = base64.b64encode(encoded_processed_image).decode('utf-8')
-        
-#         return jsonify({'image': processed_image})
-#     except Exception as e:
-#         print("Error selecting rock point:", e)
-#         return jsonify({'error': 'Error selecting rock point'})
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000)
-
-
 from flask import Flask, request, jsonify
 import cv2
 import numpy as np
@@ -129,114 +6,56 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/process_image', methods=['POST'])
-def process_image():
-    try:
-        data = request.json
-        encoded_image = data['image']
-        
-        # 解碼圖像
-        decoded_image = base64.b64decode(encoded_image)
-        np_img = np.frombuffer(decoded_image, np.uint8)
-        img = cv2.imdecode(np_img, cv2.IMREAD_COLOR)
-        
-        # 保存原始圖像以供後續使用
-        cv2.imwrite('original_image.png', img)
+def process_image_by_color(img, color):
+    color_ranges = {
+        'red': ([0, 50, 50], [10, 255, 255]),
+        'orange': ([10, 50, 50], [20, 255, 255]),
+        'yellow': ([20, 50, 50], [35, 255, 255]),
+        'green': ([35, 50, 50], [90, 255, 255]),
+        'blue': ([90, 50, 50], [140, 255, 255]),
+        'purple': ([140, 50, 50], [170, 255, 255]),
+        'black': ([0, 0, 0], [180, 255, 30]),
+    }
 
-        # 將圖像轉換為HSV色彩空間
-        hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    if color not in color_ranges:
+        return None, None
 
-        # 定義顏色範圍
-        lower_color = np.array([40, 50, 50])
-        upper_color = np.array([90, 255, 255])
-
-        # 過濾岩點
-        color_mask = cv2.inRange(hsv_img, lower_color, upper_color)
-        color_points = cv2.bitwise_and(img, img, mask=color_mask)
-
-        # 在透明背景圖像上繪製描邊框線
-        contours, _ = cv2.findContours(color_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cv2.drawContours(color_points, contours, -1, (255, 0, 0), thickness=2)
-
-        # 創建透明背景圖像
-        transparent_img = np.zeros((img.shape[0], img.shape[1], 4), dtype=np.uint8)
-
-        # 將岩點和框線添加到透明背景圖像
-        transparent_img[:, :, :3] = color_points
-        transparent_img[:, :, 3] = cv2.bitwise_or(color_mask, color_mask)
-
-        # 將透明背景圖像疊加到原始圖像上
-        overlay = cv2.addWeighted(img, 0.75, transparent_img[:, :, :3], 0.5, 0)
-
-        # 將處理後的圖像轉換為base64字符串
-        _, encoded_processed_image = cv2.imencode('.png', overlay)
-        processed_image = base64.b64encode(encoded_processed_image).decode('utf-8')
-
-        return jsonify({'image': processed_image})
-    except Exception as e:
-        print("Error processing image:", e)
-        return jsonify({'error': 'Error processing image'})
-
+    lower_color, upper_color = color_ranges[color]
+    lower_color = np.array(lower_color)
+    upper_color = np.array(upper_color)
+    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    color_mask = cv2.inRange(hsv_img, lower_color, upper_color)
+    color_points = cv2.bitwise_and(img, img, mask=color_mask)
+    contours, _ = cv2.findContours(color_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(color_points, contours, -1, (255, 0, 0), thickness=2)
+    transparent_img = np.zeros((img.shape[0], img.shape[1], 4), dtype=np.uint8)
+    transparent_img[:, :, :3] = color_points
+    transparent_img[:, :, 3] = cv2.bitwise_or(color_mask, color_mask)
+    overlay = cv2.addWeighted(img, 0.75, transparent_img[:, :, :3], 0.5, 0)
+    _, encoded_processed_image = cv2.imencode('.png', overlay)
+    processed_image = base64.b64encode(encoded_processed_image).decode('utf-8')
+    filename = f'{color}_result.png'
+    cv2.imwrite(os.path.join('uploads', filename), overlay)
+    return processed_image, filename
 
 @app.route('/select_rock_point', methods=['POST'])
 def select_rock_point():
     try:
         data = request.json
         color = data['color']
-        
-        # 定義顏色範圍，這裡需要你之前提供的顏色範圍
-        color_ranges = {
-            'red': ([0, 50, 50], [10, 255, 255]),
-            'orange': ([10, 50, 50], [20, 255, 255]),
-            'yellow': ([20, 50, 50], [35, 255, 255]),
-            'green': ([35, 50, 50], [90, 255, 255]),
-            'blue': ([90, 50, 50], [140, 255, 255]),
-            'purple': ([140, 50, 50], [170, 255, 255]),
-            'black': ([0, 0, 0], [180, 255, 30]),
-        }
-        
-        if color not in color_ranges:
+        encoded_image = data['image']
+        decoded_image = base64.b64decode(encoded_image)
+        np_img = np.frombuffer(decoded_image, np.uint8)
+        img = cv2.imdecode(np_img, cv2.IMREAD_COLOR)
+        processed_image, filename = process_image_by_color(img, color)
+        if processed_image is None:
             return jsonify({'error': 'Invalid color selected'}), 400
-
-        lower_color, upper_color = color_ranges[color]
-        lower_color = np.array(lower_color)
-        upper_color = np.array(upper_color)
-        
-        # 讀取原始圖像
-        img = cv2.imread('original_image.png')
-        hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        
-        # 過濾指定顏色的岩點
-        color_mask = cv2.inRange(hsv_img, lower_color, upper_color)
-        color_points = cv2.bitwise_and(img, img, mask=color_mask)
-        
-        # 在透明背景圖像上繪製描邊框線
-        contours, _ = cv2.findContours(color_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cv2.drawContours(color_points, contours, -1, (255, 0, 0), thickness=2)
-        
-        # 創建透明背景圖像
-        transparent_img = np.zeros((img.shape[0], img.shape[1], 4), dtype=np.uint8)
-        
-        # 將岩點和框線添加到透明背景圖像
-        transparent_img[:, :, :3] = color_points
-        transparent_img[:, :, 3] = cv2.bitwise_or(color_mask, color_mask)
-        
-        # 將透明背景圖像疊加到原始圖像上
-        overlay = cv2.addWeighted(img, 0.75, transparent_img[:, :, :3], 0.5, 0)
-        
-        # 將處理後的圖像轉換為base64字符串
-        _, encoded_processed_image = cv2.imencode('.png', overlay)
-        processed_image = base64.b64encode(encoded_processed_image).decode('utf-8')
-
-        # 保存處理後的圖像
-        filename = f'{color}_result.png'
-        cv2.imwrite(filename, overlay)
-        
         return jsonify({'image': processed_image, 'filename': filename})
-        # return jsonify({'image': processed_image})
     except Exception as e:
         print("Error processing image:", e)
         return jsonify({'error': 'Error processing image'})
 
 if __name__ == '__main__':
+    if not os.path.exists('uploads'):
+        os.makedirs('uploads')
     app.run(host='0.0.0.0', port=5000)
