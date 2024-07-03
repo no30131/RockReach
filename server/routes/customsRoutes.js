001980c14ce1ce3387dc6cf69749d1934fe0a622
Melody <no30131@gmail.com>
2024-07-03 15:23:07 +0800

    [modify] update frontend pages and bugs fixing - custom 3

--- server/routes/customsRoutes.js
+++ server/routes/customsRoutes.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const customsController = require("../controllers/customsController");
-const upload = require("../config/multerConfig");
+// const upload = require("../config/multerConfig");
 
-router.post("/create", upload.single('image'), customsController.createCustoms);
-router.post("/process", upload.single('image'), customsController.processImage);
+// router.post("/create", upload.single('image'), customsController.createCustoms);
+// router.post("/process", upload.single('image'), customsController.processImage);
 
-// router.post("/create", customsController.createCustoms);
-// router.post("/process", customsController.processImage);
+router.post("/create", customsController.createCustoms);
+router.post("/process", customsController.processImage);
 router.get("/walls", customsController.getCustomsWalls);
 router.get("/walls/:wallName", customsController.getCustomsWallRoutes);
 router.get("/walls/share/:id", customsController.getCustomsWallRouteById);
